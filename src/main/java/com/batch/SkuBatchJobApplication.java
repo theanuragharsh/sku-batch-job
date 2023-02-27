ba0f2546f41405aa84d0ae90e917dd595ca7e699
theanuragharsh <nrgharsh@gmail.com>
2023-02-27 16:19:50 +0530

    Initial commit

---
+++ src/main/java/com/batch/SkuBatchJobApplication.java
@@ -0,0 +1,13 @@
+package com.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkuBatchJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkuBatchJobApplication.class, args);
+	}
+
+}
