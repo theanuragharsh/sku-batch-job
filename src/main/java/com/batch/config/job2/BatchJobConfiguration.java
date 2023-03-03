b0e488be7f26c9aa973fbc20acc699225ebaf8d2
theanuragharsh <nrgharsh@gmail.com>
2023-03-03 17:48:44 +0530

    created Step3 for exporting data to xml file using csvWriter

--- src/main/java/com/batch/config/job2/BatchJobConfiguration.java
+++ src/main/java/com/batch/config/job2/BatchJobConfiguration.java
@@ -17,10 +17,15 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.data.domain.Sort;
 
 import javax.sql.DataSource;
@@ -48,7 +53,7 @@ public class BatchJobConfiguration {
 
     @Bean
     public Job updatePriceByTenPercentJob() {
-        return jobBuilderFactory.get("updatePriceByTenPercentJob").incrementer(new RunIdIncrementer()).start(step1()).next(step2()).next(step3()).build();
+        return jobBuilderFactory.get("updatePriceByTenPercentJob").incrementer(new RunIdIncrementer()).start(step1()).next(step2()).next(step3()).next(step5()).build();
     }
 
     @Bean
@@ -161,5 +166,34 @@ public class BatchJobConfiguration {
                 .build();
     }
 
+    @Bean
+    public FlatFileItemWriter<CatalogueItemJob> csvWriter() {
+        BeanWrapperFieldExtractor<CatalogueItemJob> fieldExtractor = new BeanWrapperFieldExtractor<>();
+        fieldExtractor.setNames(new String[]{"id", "skuNumber", "itemName", "Description", "category", "price", "inventory"}); // add other field names here as needed
+        DelimitedLineAggregator<CatalogueItemJob> lineAggregator = new DelimitedLineAggregator<>();
+        lineAggregator.setDelimiter(",");
+        lineAggregator.setFieldExtractor(fieldExtractor);
+        FlatFileItemWriter<CatalogueItemJob> writer = new FlatFileItemWriter<>();
+        writer.setLineAggregator(lineAggregator);
+        writer.setResource(new FileSystemResource("CatalogueItemJob.csv")); // specify the output file path here
+        writer.setHeaderCallback(writer1 ->
+                writer1.write("ID, skuNumber, Item Name, Description, category, price, inventory")// add other field names here as needed
+        );
+        return writer;
+    }
+
+    @Bean
+    public Step step5() {
+        return this.stepBuilderFactory.get("step5")
+                .<CatalogueItemJob, CatalogueItemJob>chunk(10)
+                .reader(new RepositoryItemReaderBuilder<CatalogueItemJob>()
+                        .name("outputRepositoryItemReader")
+                        .repository(catalogueItemJobRepo)
+                        .methodName("findAll")
+                        .sorts(Collections.singletonMap("id", Sort.Direction.ASC))
+                        .build())
+                .writer(csvWriter())
+                .build();
+    }
 
 }
